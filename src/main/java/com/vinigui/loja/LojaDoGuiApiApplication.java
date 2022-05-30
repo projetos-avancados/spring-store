b9ea852d988190b67d0bdde9c7fafd1242a920c0
vnniciu <vnniciussantos@gmail.com>
2022-05-30 11:29:08 -0300

    Commit inicial projeto Spring boot

---
+++ src/main/java/com/vinigui/loja/LojaDoGuiApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinigui.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDoGuiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDoGuiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinigui/loja/LojaDoGuiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinigui.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDoGuiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
